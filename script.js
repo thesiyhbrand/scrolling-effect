56a82b66eabab1ba79e1a6a44384eb8d80b6f480
Amarjeet Chaudhary <siyhcompany@gmail.com>
2022-11-08 14:19:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var tl = gsap.timeline({scrollTrigger:{
+    trigger:"#main",
+    // markers:true,
+    start:"38% 50%",
+    end:"100% 50%",
+    scrub:2,
+    pin:true
+}});
+tl
+.to(".text",{
+    top: "-7%",
+},'a')
+.to("#card-one",{
+    top: "35%",
+},'a')
+.to("#card-two",{
+    top: "130%"
+},'a')
+.to("#card-two",{
+    top: "42%"
+},'b')
+.to("#card-one",{
+    width: "65%",
+    height: "65vh"
+},'b')
+.to("#card-three",{
+    top: "130%"
+}, 'b')
+.to("#card-three",{
+    top: "50%"
+}, 'c')
+.to("#card-two",{
+    width: "70%",
+    height: "70vh"
+},'c')
